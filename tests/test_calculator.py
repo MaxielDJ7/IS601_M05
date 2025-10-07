e4493087edab580336eaffaffa01d01a29d4bdbf
Maxiel De Jesus <mid6@njit.edu>
2025-10-06 22:16:17 -0400

    added calculator_memento.py tests.

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -11,6 +11,8 @@ from app.calculator_config import CalculatorConfig
 from app.exceptions import OperationError, ValidationError
 from app.history import LoggingObserver, AutoSaveObserver
 from app.operations import OperationFactory
+from app.calculator_memento import CalculatorMemento
+from app.calculation import Calculation
 
 # Fixture to initialize Calculator with a temporary directory for file paths
 @pytest.fixture
@@ -214,3 +216,48 @@ def test_history_trims_to_max_size(calculator):
     
     assert calculator.history[0].operand1 == Decimal("2")
     assert calculator.history[1].operand1 == Decimal("3")
+
+# Calculator_memento.py tests
+
+def test_memento_to_dict_returns_serialized_state():
+    """Ensure CalculatorMemento.to_dict() serializes history and timestamp properly."""
+    
+    # Arrange
+    calc = Calculation(operation="Addition", operand1=Decimal("2"), operand2=Decimal("3"))
+    
+    # Act
+    memento = CalculatorMemento(history=[calc])
+    result = memento.to_dict()
+
+    # Assert
+    assert isinstance(result, dict) # is result a dictionary
+    assert "history" in result # is the string history in result
+    assert "timestamp" in result # is the string timestamp in result
+    assert isinstance(result["history"], list) # is history a list
+    assert isinstance(result["history"][0], dict) # is the first index of history a dictionary
+    assert result["history"][0]["operation"] == "Addition" # is the operation stored what was called
+
+    datetime.datetime.fromisoformat(result["timestamp"])  # timestamp format
+
+def test_memento_from_dict():
+    """Ensure CalculatorMemento.from_dict() restores the calculator's history and timestamp."""
+
+    # Arrange: create args for from_dict(Dict[str, Any]) 
+    calc = Calculation(operation="Addition", operand1=Decimal("2"), operand2=Decimal("3"))
+    calc_dict = calc.to_dict()
+    saved_data = {
+        "history": [calc_dict],
+        "timestamp": datetime.datetime.now().isoformat()
+    }
+
+    # Act
+    restored_memento = CalculatorMemento.from_dict(saved_data)
+
+    # Assert
+    assert isinstance(restored_memento, CalculatorMemento) # is act an instance of memento
+    assert isinstance(restored_memento.history, list) # is history a list
+    assert isinstance(restored_memento.history[0], Calculation) # is first history index a calculation object
+    assert restored_memento.history[0].operation == "Addition" # is the operation stored what was called
+    assert restored_memento.history[0].result == Decimal("5")
+    assert isinstance(restored_memento.timestamp, datetime.datetime)
+    assert restored_memento.timestamp.isoformat() == saved_data["timestamp"]
